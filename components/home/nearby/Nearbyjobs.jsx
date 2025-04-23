599059bfa592c0fa07b62663b0b253a1253d6eec
Taylor Stephenson <stephenson.taylor@gmail.com>
2025-04-23 15:06:04 -0500

    Continued to build out the Nearby jobs

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -34,7 +34,9 @@ const Nearbyjobs = () => {
         ) : (
           data?.map((job) => (
             <NearbyJobCard
-            
+            job={job}
+            key={`nearby-job-${job?.job_id}`}
+            handleNavigate={() => router.push(`/job-deatails/${job.job_id}`)}
             />
           ))  
         )}
